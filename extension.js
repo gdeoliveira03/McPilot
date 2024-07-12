5826317ddbede577b6e5465d5633fcb8a2eb582e
Gabriel De Oliveira <91763970+gdeoliveira03@users.noreply.github.com>
2024-07-12 09:59:56 -0400

    no more proj within the proj

--- extension.js
+++ extension.js
@@ -17,11 +17,11 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mcpilot.mcPilot', function () {
+	const disposable = vscode.commands.registerCommand('mcpilot.mcpilotLives', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('mcpilot is alive and well');
+		vscode.window.showInformationMessage('Were alive!');
 	});
 
 	context.subscriptions.push(disposable);
