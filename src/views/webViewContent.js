06325502a31af73e9967465a45632aebc3c044a1
Alison Chu <alichu1@publicisgroupe.net>
2024-07-22 10:57:56 -0400

    ensured all options for dropbox region

--- src/views/webViewContent.js
+++ src/views/webViewContent.js
@@ -46,12 +46,15 @@ function getWebviewContent(panel) {
     <div class="input-group">
       <label>AWS Region:</label>
       <select id="awsRegion">
-        <option value="us-east-1">US East (N. Virginia)</option>
         <option value="us-east-2">US East (Ohio)</option>
+        <option value="us-east-1">US East (Virginia)</option>
         <option value="us-west-1">US West (N. California)</option>
         <option value="us-west-2">US West (Oregon)</option>
         <option value="af-south-1">Africa (Cape Town)</option>
         <option value="ap-east-1">Asia Pacific (Hong Kong)</option>
+        <option value="ap-south-2">Asia Pacific (Hyderabad)</option>
+        <option value="ap-southeast-3">Asia Pacific (Jakarta)</option>
+        <option value="ap-southeast-4">Asia Pacific (Melbourne)</option>
         <option value="ap-south-1">Asia Pacific (Mumbai)</option>
         <option value="ap-northeast-3">Asia Pacific (Osaka)</option>
         <option value="ap-northeast-2">Asia Pacific (Seoul)</option>
@@ -59,13 +62,18 @@ function getWebviewContent(panel) {
         <option value="ap-southeast-2">Asia Pacific (Sydney)</option>
         <option value="ap-northeast-1">Asia Pacific (Tokyo)</option>
         <option value="ca-central-1">Canada (Central)</option>
+        <option value="ca-west-1">Canada West (Calgary)</option>
         <option value="eu-central-1">Europe (Frankfurt)</option>
         <option value="eu-west-1">Europe (Ireland)</option>
         <option value="eu-west-2">Europe (London)</option>
         <option value="eu-south-1">Europe (Milan)</option>
         <option value="eu-west-3">Europe (Paris)</option>
+        <option value="eu-south-2">Europe (Spain)</option>
         <option value="eu-north-1">Europe (Stockholm)</option>
+        <option value="eu-central-2">Europe (Zurich)</option>
+        <option value="il-central-1">Israel (Tel Aviv)</option>
         <option value="me-south-1">Middle East (Bahrain)</option>
+        <option value="me-central-1">Middle East (UAE)</option>
         <option value="sa-east-1">South America (São Paulo)</option>
       </select>
     </div>
