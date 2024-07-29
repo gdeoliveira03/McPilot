62f9eb50190f0166b1e1af3c74c8b8180d1e9c70
Gabriel De Oliveira <91763970+gdeoliveira03@users.noreply.github.com>
2024-07-29 10:13:30 -0400

    preprocessing fixx

--- src/utils/prompt.js
+++ src/utils/prompt.js
@@ -1,14 +1,16 @@
 function preprocessPrompt(rawPrompt) {
     // Template for generating Terraform code with instructions for the AI
     const template = `
-      You are an expert in generating Terraform templates. The user will provide a description of what the Terraform template should do. Please generate a \`.tf\` file based on the user's description with the following requirements:
+      You are an expert in generating Terraform templates. The user will provide a description of what the Terraform template should do. Please generate a .tf file based on the user's description with the following requirements:
   
       1. The output must be valid Terraform code.
-      2. Only include code in the output. Any explanations or non-code content should be commented out with a \`#\`.
+      2. Only include code in the output. Any explanations or non-code content should not be present.
       3. Ensure that all necessary resources, configurations, and permissions are included.
       4. Use AWS as the cloud provider.
-      5. Provide comments and explanations within the code using the \`#\` symbol.
-  
+      5. DO NOT use code block delimiters like \`\`\`terraform or \`\`\`.
+      6. DO NOT include any comments or explanations within the code.
+
+
       Here is the user's description:
       "${rawPrompt}"
   
@@ -18,6 +20,6 @@ function preprocessPrompt(rawPrompt) {
     return template.trim();
 }
   
-  module.exports = {
+module.exports = {
     preprocessPrompt,
 };
