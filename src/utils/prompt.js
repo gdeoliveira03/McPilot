daf8b038eea02b21985142a96fd0f3586440995c
KimHsuehPS <kim.hsueh@publicissapient.com>
2024-07-30 14:58:26 -0400

    added the seventh bullet point to the preprocessing prompt

--- src/utils/prompt.js
+++ src/utils/prompt.js
@@ -1,6 +1,6 @@
 function preprocessPrompt(rawPrompt) {
-    // Template for generating Terraform code with instructions for the AI
-    const template = `
+  // Template for generating Terraform code with instructions for the AI
+  const template = `
       You are an expert in generating Terraform templates. The user will provide a description of what the Terraform template should do. Please generate a .tf file based on the user's description with the following requirements:
 
       1. The output must be valid Terraform code.
@@ -9,7 +9,7 @@ function preprocessPrompt(rawPrompt) {
       4. Use AWS as the cloud provider.
       5. DO NOT use code block delimiters like \`\`\`terraform or \`\`\`.
       6. DO NOT include any comments or explanations within the code.
-
+      7. Ensure that there are NO duplicate instances.
 
       Here is the user's description:
       "${rawPrompt}"
@@ -20,10 +20,10 @@ function preprocessPrompt(rawPrompt) {
 
       Generate the Terraform template below:
     `;
-  
-    return template.trim();
+
+  return template.trim();
 }
-  
+
 module.exports = {
-    preprocessPrompt,
+  preprocessPrompt,
 };
